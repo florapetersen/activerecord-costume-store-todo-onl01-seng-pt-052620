6fe595741045f4b718c8cd490e4cf7b3d0f41b55
Flora Petersen <floramarina@comcast.net>
2020-08-15 21:10:32 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :start_time
-      t.datetime :end_time 
+      t.datetime :closing_time 
     end
   end
 end
