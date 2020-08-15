8a108768cf13f7e35a6b51ca3f78da70eafa5f25
Flora Petersen <floramarina@comcast.net>
2020-08-15 21:12:49 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.boolean :family_friendly
       t.string :opening_date
       t.string :closing_date
-      t.string :long_description
+      t.description :long_description
     end
   end
 end
