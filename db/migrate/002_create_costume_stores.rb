ba10f06c3a0298f968ae65afbd7e327a96e6c1cf
Flora Petersen <floramarina@comcast.net>
2020-08-15 20:24:56 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,7 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
   def change
     create_table :costume_stores do |t|
+      t.string :name
       
+
 end# Create your costume_stores migration here
